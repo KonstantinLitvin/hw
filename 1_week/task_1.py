56d793f6860cde675b8ddbdb73e2dd4a04da94da
KonstantinLitvin <konstantinlitvin89@gmail.com>
2020-10-13 00:21:12 +0300

    added tests

--- 1_week/task_1.py
+++ 1_week/task_1.py
@@ -1,15 +1,9 @@
 import hashlib
-import random
-import string
-import tempfile
-import pathlib
 from collections import defaultdict
 from pathlib import Path
 from pprint import pprint
-from typing import List
-import os
 
-tmp_dir = 'tmp'
+tmp_dir = '../tests/tmp'
 
 
 def get_hash(path_to_file: Path) -> str:
@@ -20,55 +14,25 @@ def get_hash(path_to_file: Path) -> str:
     return file_hash.hexdigest()
 
 
-class Test:
-    files: List[str] = []
+def find_duplicates(path, check_mode=True):
+    p = Path(path)
 
-    @staticmethod
-    def get_content(k=100) -> bytes:
-        return str.encode(''.join(random.choices(string.ascii_uppercase + string.digits, k=k)))
+    hs = defaultdict(list)
+    for f in p.iterdir():
+        h = get_hash(f)
+        hs[h].append(f)
+        print(f"f: {f}, {h}")
+    pprint(hs)
 
-    @staticmethod
-    def generate_test_files(n: int = 100) -> None:
-        """
-        Generate n files with random content. Each even file is duplicate of the previous file
-        :param n: files number
-        """
-        content = Test.get_content()
+    for k, v in hs.items():
+        print(k)
+        fn_original: Path = v.pop()
+        print(f"fn_original: {fn_original}")
+        while (l := len(v)) > 0:
+            fn: Path = v.pop()
 
-        for i in range(n):
-
-            if i % 2 == 0:
-                content = Test.get_content()
-            file = tempfile.NamedTemporaryFile(dir=tmp_dir, delete=False)
-            file.write(content)
-            file.flush()
-
-            Test.files.append(file.name)
-
-            file.close()
-
-
-Test.generate_test_files()
-
-p = Path('tmp')
-
-hs = defaultdict(list)
-for f in p.iterdir():
-    h = get_hash(f)
-    hs[h].append(f)
-    print(f"f: {f}, {h}")
-pprint(hs)
-
-
-for k, v in hs.items():
-    print(k)
-    fn_original: Path = v.pop()
-    print(f"fn_original: {fn_original}")
-    while (l := len(v)) > 0:
-        fn: Path = v.pop()
-
-        # print(f"fn: {fn}")
-        # fn.unlink()
-        print(fn_original.samefile(fn))
-    print(v)
-    print()
+            # print(f"fn: {fn}")
+            # fn.unlink()
+            print(fn_original.samefile(fn))
+        print(v)
+        print()
---
+++ tests/task_1_test.py
@@ -0,0 +1,35 @@
+import random
+import string
+import tempfile
+from typing import List
+
+
+class Test:
+    files: List[str] = []
+
+    @staticmethod
+    def get_content(k=100) -> bytes:
+        return str.encode(''.join(random.choices(string.ascii_uppercase + string.digits, k=k)))
+
+    @staticmethod
+    def generate_test_files(n: int = 100) -> None:
+        """
+        Generate n files with random content. Each even file is duplicate of the previous file
+        :param n: files number
+        """
+        content = Test.get_content()
+
+        for i in range(n):
+
+            if i % 2 == 0:
+                content = Test.get_content()
+            file = tempfile.NamedTemporaryFile(dir=tmp_dir, delete=False)
+            file.write(content)
+            file.flush()
+
+            Test.files.append(file.name)
+
+            file.close()
+
+
+Test.generate_test_files()
